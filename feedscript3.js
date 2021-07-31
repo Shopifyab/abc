229ab009704a470b73175a7d0f53cf6e28d00260
Shopifyab <70981808+Shopifyab@users.noreply.github.com>
2021-07-31 19:21:07 -0400

    Create feedscript3.js

---
+++ feedscript3.js
@@ -0,0 +1,22 @@
+  
+(function () {
+
+    // make api call to get board from dynamodb
+  console.log("1")
+  function getStatus(callback){
+      console.log("2", window)
+
+   if(window.location.href.indexOf('pfeedid=') > 0){
+      console.log("has the pfeedid")
+      } else {
+      console.log("no pid")
+      }
+          console.log("3")
+
+  }
+            console.log("4")
+
+    getStatus()
+              console.log("5")
+
+        })
